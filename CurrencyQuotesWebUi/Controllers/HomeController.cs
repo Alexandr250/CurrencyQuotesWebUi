9f3dc7b9d1d9609fc3507ea9d82d2521c4963d19
Alexandr250 <Alexandr250@bk.ru>
2020-02-14 19:00:45 +0300

    _

---
+++ CurrencyQuotesWebUi/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CurrencyQuotesCore;
+
+namespace CurrencyQuotesWebUi.Controllers {
+    public class HomeController : Controller {
+        public IActionResult Index() {
+            ExchangeRatesLocalRepository repository = new ExchangeRatesLocalRepository( new System.IO.FileInfo( "daily.json" ) );
+            return View( repository.WithFilter( null ) );
+        }
+    }
+}
---
+++ CurrencyQuotesWebUi/wwwroot/js/table.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    //$('#scroolable-table').DataTable({
+    //    "scrollX": true,
+    //    "scrollY": 200,
+    //});
+    //$('.dataTables_length').addClass('bs-select');
+    $("#selectable-table").selectable();
+});
