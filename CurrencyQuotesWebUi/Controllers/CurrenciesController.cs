6108b73b902f9a2c14267f0c523164a95f021f07
Alexandr250 <Alexandr250@bk.ru>
2020-02-17 20:25:12 +0300

    _

---
+++ CurrencyQuotesWebUi/Controllers/CurrenciesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CurrencyQuotesCore;
+using CurrencyQuotesCore.Entites;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CurrencyQuotesWebUi.Controllers {
+    [Route( "api/[controller]" )]
+    public class CurrenciesController : Controller {
+        // GET: api/<controller>
+        [HttpGet]
+        public IEnumerable<Currency> Get() {
+            ExchangeRatesLocalRepository repository = new ExchangeRatesLocalRepository( new System.IO.FileInfo( "daily.json" ) );
+            return repository.WithFilter( null );
+        }
+
+        // GET: api/<controller>
+        [HttpGet( "{filter}" )]
+        public IEnumerable<Currency> Get( string filter ) {
+            ExchangeRatesLocalRepository repository = new ExchangeRatesLocalRepository( new System.IO.FileInfo( "daily.json" ) );
+            return repository.WithFilter( filter );
+        }
+
+        // GET api/<controller>/5
+        //[HttpGet( "{id}" )]
+        //public string Get( int id ) {
+        //    return "value";
+        //}
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post( [FromBody]string value ) {
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut( "{id}" )]
+        public void Put( int id, [FromBody]string value ) {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete( "{id}" )]
+        public void Delete( int id ) {
+        }
+    }
+}
--- CurrencyQuotesWebUi/wwwroot/js/table.js
+++ CurrencyQuotesWebUi/wwwroot/js/table.js
@@ -1,8 +1,31 @@
-﻿$(document).ready(function () {
-    //$('#scroolable-table').DataTable({
-    //    "scrollX": true,
-    //    "scrollY": 200,
-    //});
-    //$('.dataTables_length').addClass('bs-select');
+﻿$ (document).ready( function () {
     $("#selectable-table").selectable();
+
+    function CurrenciesViewModel() {
+        var self = this;
+
+        self.filter = ko.observable();;
+        self.currencies = ko.observableArray();
+
+        self.getData = function () {
+            var innerFilter = typeof self.filter() == "undefined" ? "" : self.filter();
+            $.get('/api/currencies/' + innerFilter).done(function (json) {
+                self.currencies.removeAll();
+                for (var i = 0; i < json.length; i++)
+                    self.currencies.push(json[i]);
+            });
+        }
+
+        self.filter.subscribe(function (newValue) {
+            self.getData();
+        });
+
+        self.getData();
+    };
+
+    ko.applyBindings(new CurrenciesViewModel());
 });
+
+
+
+
